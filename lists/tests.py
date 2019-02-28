d984f05897f853c40198522b5aa335666c2b9578
kstuve123 <kstuve12@naver.com>
2019-02-28 15:20:23 +0900

    Refactor home page view to use a temlate

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,7 @@
-from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
-from lists.views import home_page
+from .views import home_page
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -11,9 +10,14 @@ class HomePageTest(TestCase):
 
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        #request = HttpRequest()
+        #response = home_page(request)
+
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>일정관리</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 home_page = None
 
 def home_page(request):
-    return HttpResponse('<html><title>일정관리</title></html>')
+    return render(request,'home.html')

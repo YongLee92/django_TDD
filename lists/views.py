bc56564c1d2ed1ae7cc85756173dce2a39508ca9
kstuve123 <kstuve12@naver.com>
2019-02-28 13:17:39 +0900

    Basic view now returns minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 home_page = None
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>일정관리</title></html>')
